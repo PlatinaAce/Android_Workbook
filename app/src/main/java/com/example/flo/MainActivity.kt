422ef53a069236abc200b704f56a68364bb05b8d
Ace <02kimjiwon@naver.com>
2024-04-11 23:36:17 +0900

    Week 2 Sumbission(Fixed)

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.flo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.flo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +17,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+
+        binding.mainPlayerCl.setOnClickListener {
+            //startActivity(Intent(this, SongActivity::class.java))
+            val intent = Intent(this,SongActivity::class.java)
+            intent.putExtra("title", song.title)
+            intent.putExtra("singer", song.singer)
+            startActivity(intent)
+        }
         initBottomNavigation()
 
+        Log.d("Song", song.title + song.singer)
     }
 
     private fun initBottomNavigation(){
@@ -55,4 +69,6 @@ class MainActivity : AppCompatActivity() {
             false
         }
     }
+
+
 }
---
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -0,0 +1,46 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flo.databinding.ActivitySongBinding
+
+class SongActivity : AppCompatActivity(){
+
+    lateinit var binding: ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.songDownIb.setOnClickListener{
+            finish()
+            //MainActivity 에서 String 타입의 값을 받으면 그 내용으로 Toast 메시지 띄우기
+            Toast.makeText(this, intent.getStringExtra("title"), Toast.LENGTH_SHORT).show()
+        }
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+
+        // 미니플레이어에 있는 제목과 가수명 SongActivity에 전달하기
+        if (intent.hasExtra("title") && intent.hasExtra("singer")){
+            binding.songMusicTitleTv.text = intent.getStringExtra("title")
+            binding.songSingerNameTv.text = intent.getStringExtra("singer")
+        }
+    }
+
+    fun setPlayerStatus(isPlaying: Boolean){
+        if (isPlaying){
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        }
+        else{
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        }
+    }
+}
